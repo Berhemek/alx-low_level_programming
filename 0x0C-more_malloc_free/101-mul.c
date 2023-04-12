7f2ed4f69674a6ec48aacde249d71cca4ce1a56c
Berhe Mekonen <berhemek21@gmail.com>
2023-04-12 22:18:14 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,25 +1,25 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 
 /**
- *  * _is_zero - determines if any number is zero
- *   * @argv: argument vector.
- *    *
- *     * Return: no return.
- *      */
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
+ *
+ * Return: no return.
+ */
 void _is_zero(char *argv[])
 {
 	int i, isn1 = 1, isn2 = 1;
-	
+
 	for (i = 0; argv[1][i]; i++)
 		if (argv[1][i] != '0')
 		{
 			isn1 = 0;
 			break;
 		}
-	
+
 	for (i = 0; argv[2][i]; i++)
 		if (argv[2][i] != '0')
 		{
@@ -35,12 +35,12 @@ void _is_zero(char *argv[])
 }
 
 /**
- *  * _initialize_array - set memery to zero in a new array
- *   * @ar: char array.
- *    * @lar: length of the char array.
- *     *
- *      * Return: pointer of a char array.
- *       */
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
 char *_initialize_array(char *ar, int lar)
 {
 	int i = 0;
@@ -52,13 +52,13 @@ char *_initialize_array(char *ar, int lar)
 }
 
 /**
- *  * _checknum - determines length of the number
- *   * and checks if number is in base 10.
- *    * @argv: arguments vector.
- *     * @n: row of the array.
- *      *
- *       * Return: length of the number.
- *        */
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
 int _checknum(char *argv[], int n)
 {
 	int ln;
@@ -69,22 +69,23 @@ int _checknum(char *argv[], int n)
 			printf("Error\n");
 			exit(98);
 		}
+
 	return (ln);
 }
 
 /**
- *  * main - Entry point.
- *   * program that multiplies two positive numbers.
- *    * @argc: number of arguments.
- *     * @argv: arguments vector.
- *      *
- *       * Return: 0 - success.
- *        */
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
+ */
 int main(int argc, char *argv[])
 {
 	int ln1, ln2, lnout, add, addl, i, j, k, ca;
 	char *nout;
-	
+
 	if (argc != 3)
 		printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
