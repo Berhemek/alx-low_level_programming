f0e8920fe0d99778b821ace0a8ebb3558e29d5fb
Berhe Mekonen <berhemek21@gmail.com>
2023-06-19 04:43:07 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -o /tmp/nrandom.so https://github.com/Berhemek/alx-low_level_programming/blob/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp/ https://github.com/Berhemek/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gm
+export LD_PRELOAD=/tmp/gm
